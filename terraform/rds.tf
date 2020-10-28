6f2b0212b060edc7b3bd9be87c8316f1d0f5fdee
Stiven Avila <stiven140@hotmail.com>
2020-10-28 13:18:17 -0500

    Add main rds instance (#2)

---
+++ terraform/rds.tf
@@ -0,0 +1,12 @@
+
+resource "aws_db_instance" "main" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "main"
+  username             = "new-architectures"
+  password             = "new-architectures"
+  parameter_group_name = "default.mysql5.7"
+}
